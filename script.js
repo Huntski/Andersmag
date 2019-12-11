69823bc13124a9698c2bb4d074b20ea2004c307f
Wiebe <wieberan@hotmail.nl>
2019-12-11 10:54:46 +0100

    Make lights flikker

--- script.js
+++ script.js
@@ -11,9 +11,15 @@ cls.forEach(e => {
 
     if (e.classList[1] === 'start')
         addrectangles = true;
-    
 })
 
 lights.forEach(e => {
-    // e.style.display = 'none';
+    e.style.display = 'none';
 })
+
+setInterval(_ => {
+    let light = lights[Math.floor(Math.random() * Math.floor(lights.length-1))]
+    console.log(light.style.display, light)
+    if (light.style.display == 'none' ) light.style.display = 'block'
+    else light.style.display = 'none'
+}, 200);
